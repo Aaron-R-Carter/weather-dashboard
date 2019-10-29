3f25d18c8239552f4a9cfaa51a4e6b1060935ad9
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-10-29 14:40:27 -0400

    javascript added to get ajax json data from openweather api and display name in city header as well as clear before replacing next search

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 var apiKey = "b735a815d875a14bc44e6e62c26375b9";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + apiKey;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
 
-    console.log(queryURL);
-    // Create CODE HERE to log the resulting object
-    // Create CODE HERE to transfer content to HTML
-    // Create CODE HERE to calculate the temperature (converted from Kelvin)
-    // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
-    // Create CODE HERE to dump the temperature content into HTML
 
-  });
+$("#search-btn-city").on("click", function (event) {
+    event.preventDefault();
+
+    // variables for city input 
+    var city = $("#city-input").val();
+    var cityQueryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    var cityHeader = $("#city-header");
+    var cityCapitalized = city.charAt(0).toUpperCase() + city.slice(1);
+
+
+    console.log(cityQueryUrl);
+
+    $.ajax({
+        url: cityQueryUrl,
+        method: "GET"
+    }).then(function (response) {
+
+        cityHeader.empty();
+        cityHeader.text(response.name);
+
+
+
+        console.log(response);
+    })
+});
+
+
+
+
+    // // Transfer content to HTML
+    // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    // $(".wind").text("Wind Speed: " + response.wind.speed);
+    // $(".humidity").text("Humidity: " + response.main.humidity);
+    // $(".temp").text("Temperature (F) " + response.main.temp);
+
+    // // Converts the temp to Kelvin with the below formula
+    // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    // $(".tempF").text("Temperature (Kelvin) " + tempF);
+
+    // // Log the data in the console as well
+    // console.log("Wind Speed: " + response.wind.speed);
+    // console.log("Humidity: " + response.main.humidity);
+    // console.log("Temperature (F): " + response.main.temp);
+
+
