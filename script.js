a098b8afc7e26227d0cb5bda933586138ae42923
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2019-10-29 14:57:36 -0400

    current date prints with search using momentjs

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// global variables
 var apiKey = "b735a815d875a14bc44e6e62c26375b9";
 
+//date function
+function todaysDate() {
+    var todaysDate = moment().format("MMMM Do YYYY");
+    $("#city-header-date").append(`<div>${todaysDate}</div>`);
+}
+
+//run date function
 
 
+// city search button click event function
 $("#search-btn-city").on("click", function (event) {
     event.preventDefault();
 
@@ -9,20 +18,20 @@ $("#search-btn-city").on("click", function (event) {
     var city = $("#city-input").val();
     var cityQueryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     var cityHeader = $("#city-header");
-    var cityCapitalized = city.charAt(0).toUpperCase() + city.slice(1);
 
-
-    console.log(cityQueryUrl);
+    // ajax call for weather api data
 
     $.ajax({
         url: cityQueryUrl,
         method: "GET"
     }).then(function (response) {
 
+        // clear previous search results 
         cityHeader.empty();
+
+        // add results to page using from response object
         cityHeader.text(response.name);
-
-
+        todaysDate();
 
         console.log(response);
     })
@@ -37,13 +46,7 @@ $("#search-btn-city").on("click", function (event) {
     // $(".humidity").text("Humidity: " + response.main.humidity);
     // $(".temp").text("Temperature (F) " + response.main.temp);
 
-    // // Converts the temp to Kelvin with the below formula
-    // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-    // $(".tempF").text("Temperature (Kelvin) " + tempF);
+ 
 
-    // // Log the data in the console as well
-    // console.log("Wind Speed: " + response.wind.speed);
-    // console.log("Humidity: " + response.main.humidity);
-    // console.log("Temperature (F): " + response.main.temp);
-
+ 
 
